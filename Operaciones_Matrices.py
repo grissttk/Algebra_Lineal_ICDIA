c49d3d5a60c01f01d34372b41b6b554a79fe1968
Griselda Luna García <grisluga05@gmail.com>
2025-11-14 19:51:02 -0600

    Add files via upload

--- Operaciones_Matrices.py
+++ Operaciones_Matrices.py
@@ -3,15 +3,39 @@ import numpy as np
 
 #SUMA
 print("SUMA")
-AS = np.array([[3, 2], [1, 4]])
-BS = np.array([[1, 3], [2, 5]])
-print(AS)
-print(BS)
+AS1 = np.array([[1, 3], [2, 5]])
+BS1 = np.array([[3, 2], [1, 4]])
+print(AS1)
+print(BS1)
+print()
 
-C = AS + BS
-print("Resultado de la suma AS + BS:")
+C = AS1 + BS1
+print("Resultado de la suma A + B:")
 print(C)
 print()
+
+AS2 = np.array([[2, 5, 1], [3, 1, 0]])
+BS2 = np.array([[1, 2, 3], [3, 4, 1]])
+print(AS2)
+print(BS2)
+print()
+
+C = AS2 + BS2
+print("Resultado de la suma A + B:")
+print(C)
+print()
+
+AS3 = np.array([[3, 2], [1, 4]])
+BS3 = np.array([[1, 3], [2, 5]])
+print(AS3)
+print(BS3)
+print()
+
+C = AS3 + BS3
+print("Resultado de la suma A + B:")
+print(C)
+print()
+
 
 #RESTA
 print("RESTA")
@@ -21,17 +45,39 @@ print(AR)
 print(BR)
 
 C = AR - BR
-print("Resultado de la resta AR - BR:")
+print("Resultado de la resta A - B:")
 print(C)
 print()
 
 #MULTIPLICACIÓN
 print("MULTIPLICACIÓN")
-AM = np.array([[3, 2, 5, 6]])
-BM = np.array([[1, 2, 4, 0]])
-print(AM)
-print(BM)
+AM1 = np.array([[3, 2, 5, 6]])
+BM1 = np.array([[1, 2, 4, 0]])
+print(AM1)
+print(BM1)
+print()
 
-C = AM * BM
-print("Resultado de la multiplicacion AM * BM:")
+C = AM1 * BM1
+print("Resultado de la multiplicacion A * B:")
+print(C)
+
+AM2 = np.array([[3, 2], [1, 4]])
+BM2 = np.array([[1, 3], [2, 5]])
+print(AM2)
+print(BM2)
+print()
+
+C = AM2 * BM2
+print("Resultado de la multiplicacion A * B:")
+print(C)
+
+print("MULTIPLICACIÓN")
+AM3 = np.array([[2, 3], [5, 1], [0,2]])
+BM3 = np.array([[4, 1, 2], [5, 2, 3]])
+print(AM3)
+print(BM3)
+print()
+
+C = AM3.dot(BM3)
+print("Multiplicacion matricial de A * B:")
 print(C)
